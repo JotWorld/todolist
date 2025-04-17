5e1bd5698ac27e9d52cb9d83497af68741900056
JotWorld <lewenka2004@gmail.com>
2025-04-17 16:42:23 +0300

    feat: deleted commits in task-board-presenter.js

--- src/presenter/tasks-board-presenter.js
+++ src/presenter/tasks-board-presenter.js
@@ -1,52 +1,3 @@
-// import TasksListComponent from "../view/task-list-component.js";
-// import TaskComponent from "../view/task-component.js";
-// import BoardComponent from "../view/task-board-component.js";
-// import { render } from "../framework/render.js";
-// import {Status, StatusLabel} from "../const/status.js";
-// import TrashButton from "../view/trash-button-component.js";
-
-// export default class TasksBoardPresenter {
-//     #boardContainer = null;
-//     #tasksModel = null;
-//     #tasksBoardComponent = new BoardComponent();
-
-//     constructor({boardContainer, tasksModel}){
-//         this.#boardContainer = boardContainer;
-//         this.#tasksModel = tasksModel;
-//     }
-
-//    init(){
-//     this.boardTasks = [...this.#tasksModel.tasks];
-//     const statuses = Object.values(Status);
-//     render(this.#tasksBoardComponent, this.#boardContainer);
-
-//     for(let i = 0;i < statuses.length; i++){
-//         const currentStatus = statuses[i];
-//         const listComponent = new TasksListComponent({
-//             title: StatusLabel[currentStatus],
-//             status: currentStatus
-//         });
-
-//         render(listComponent, this.#tasksBoardComponent.element);
-//         const taskListComponent = listComponent.element;
-
-//         const filteredTasks = this.boardTasks.filter(task => task.status === currentStatus)
-
-
-//         for(let j = 0; j < filteredTasks.length; j++){
-//             console.log(filteredTasks[j].title);
-//             const taskComponent = new TaskComponent(filteredTasks[j].title);
-            
-//             render(taskComponent, taskListComponent.querySelector('.task-column'))  
-//         }
-//         if(currentStatus == 'trash'){
-//             render(new TrashButton(),taskListComponent);
-//         }
-//     }
-//    }
-
-      
-// }
 import TasksListComponent from "../view/task-list-component.js";
 import TaskComponent from "../view/task-component.js";
 import BoardComponent from "../view/task-board-component.js";
@@ -68,17 +19,16 @@ export default class TasksBoardPresenter {
   init() {
     this.#boardTasks = [...this.#tasksModel.tasks];
     this.#renderBoard();
+  }
 
+  #renderBoard() {
+    render(this.#tasksBoardComponent, this.#boardContainer);
     const statuses = Object.values(Status);
     for (let i = 0; i < statuses.length; i++) {
       const currentStatus = statuses[i];
       const filteredTasks = this.#boardTasks.filter((task) => task.status === currentStatus);
       this.#renderTasksList(currentStatus, filteredTasks);
     }
-  }
-
-  #renderBoard() {
-    render(this.#tasksBoardComponent, this.#boardContainer);
   }
 
   #renderTasksList(status, tasks) {
@@ -113,5 +63,5 @@ export default class TasksBoardPresenter {
   #renderEmptyList(container){
     render(new EmptyTaskListComponent(), container);
   }
-  
+
 }
